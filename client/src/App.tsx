652fdc8d153c7472c661e6e6c3e829a92d12f841
Velros <bossfahad2003@gmail.com>
2025-11-15 02:39:53 +0300

    Remove unused imports from App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,4 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
